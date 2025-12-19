5dbb310635f5d7fa8ca34c543e1857e2f42ba452
beemotik <ti@beemotik.com>
2025-12-19 14:18:33 -0600

    ..

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,20 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
+  }
 });
